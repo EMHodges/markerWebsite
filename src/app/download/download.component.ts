32d1d51c3d0598d17513fc6ffe66d25e4af39eab
Elizabeth Hodges <elizabethhodges@macbook-pro.home>
2022-03-07 18:39:20 +0000

    upload form

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { DownloadService } from './download.service';
 
@@ -9,15 +10,43 @@ import { DownloadService } from './download.service';
 })
 export class DownloadComponent implements OnInit {
 
-  public downloadId = "python-marker" + ("" + Math.random()).substring(2, 8);
+  private GOOGLE_EMAIL_SCRIPT = "https://script.google.com/macros/s/AKfycbxrhGWvS5BUYW4d6-0LU_PhILJYUoqfYKYGiAqcVzhx5RdLmRQFT2GVIKMlQHQgg8qq0w/exec"
 
-  constructor(private downloadService: DownloadService) {}
+  public downloadId = "python-marker" + ("" + Math.random()).substring(2, 8);
+  fileName = '';
+
+  constructor(private downloadService: DownloadService, private http: HttpClient) {}
 
   ngOnInit(): void {}
 
   postDownload() {
     this.downloadService.sendDownloadEmail(this.downloadId)
   }
+
+  onFileSelected(event: any) {
+
+    const file:File = event.target.files[0];
+
+    if (file) {
+
+        this.fileName = file.name;
+
+        const formData = new FormData();
+
+        formData.append("thumbnail", file);
+        this.http.post(this.GOOGLE_EMAIL_SCRIPT, formData).subscribe(
+          (response) => {
+            console.log(response);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+
+        console.log(formData)
+        console.log(this.fileName)
+    }
+}
 
 }
 
